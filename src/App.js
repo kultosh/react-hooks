6bae9ae38a58e06aba5b4bc6013abb879604be58
kultosh <kulgole90@gmail.com>
2020-10-16 21:46:07 +0545

    Created counter with event using useState hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  let Increase
+  let Decrease
+
+  if(counter >= 0)
+  {
+     Increase = () => {
+      setCounter( counter + 1)
+    }
+  
+     Decrease = () => {
+      setCounter( counter - 1)
+    }
+  }
+
+  else
+  {
+    setCounter(0);
+  }
+
+
   return (
     <div>
       <h1>React Hooks & Context</h1>
+      <div>
+        <button onClick={Increase}>+</button>
+        <span>{counter}</span>
+        <button onClick={Decrease}>-</button>
+      </div>
     </div>
   );
 }
