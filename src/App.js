81a9375c63ff771693de1b3a923c6dc377d46cce
kultosh <kulgole90@gmail.com>
2020-10-17 21:56:11 +0545

    Learned to focus on some element or object using useRef hook

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import React, {useState, useRef, useEffect} from 'react';
 import './App.css'
 
 function App() {
-  const [name, setName] = useState('null');
+  const [name, setName] = useState('');
 
-  const renderCount = useRef(0);
+  const inputRef = useRef();
 
-  useEffect(() => {
-    renderCount.current = renderCount.current + 1
-  })
+  function focus() {
+    inputRef.current.focus();
+  }
 
   return (
     <div className="App">
       <h2>useRef Hook</h2>
       <div>
-        <input type="text" onChange={(e) => setName(e.target.value)} />
+        <input ref={inputRef} type="text" onChange={(e) => setName(e.target.value)} />
       </div>
       <p>My name is {name} </p>
-      <p>I rendered {renderCount.current}</p>
+    
+      <button onClick={focus}>Focus</button>
     </div>
   )
 
